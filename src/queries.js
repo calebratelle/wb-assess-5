ca2dc9bfcabfe74070aa3f567a613ee139db1f27
Caleb Ratelle <caleb.ratelle@gmail.com>
2023-09-06 15:09:02 -0600

    assessment 5 done

--- src/queries.js
+++ src/queries.js
@@ -15,8 +15,11 @@ export const query2 = await Animal.findOne({
 
 // Get all animals belonging to the human with primary key 5
 
-export const query3 = await Human.findByPk(5);
-
+export const query3 = await Animal.findAll({
+    where: {
+        human_id: 5
+    }
+})
 // Get all animals born in a year greater than (but not equal to) 2015.
 
 export const query4 = await Animal.findAll({
@@ -27,13 +30,15 @@ export const query4 = await Animal.findAll({
 
 // Get all the humans with first names that start with "J"
 export const query5 = await Human.findAll({
-    fname: {[Op.startsWith]: 'J'}
+    where: {
+        fname: {[Op.startsWith]: 'J'}
+    }
 });
 
 // Get all the animals who don't have a birth year
 export const query6 = await Animal.findAll({
     where: {
-        // birth_year: NULL
+        birth_year: null
     }
 });
 
@@ -49,7 +54,7 @@ export const query7 = await Animal.findAll({
 // Get all the humans who DON'T have an email address that contains "gmail"
 export const query8 = await Human.findAll({
     where: {
-        // [Op.not]: {email:[Op.like]: '%gmail%'}
+        email: {[Op.notLike]:'%gmail%'}
     }
 })
 
@@ -68,17 +73,18 @@ export async function printHumansAndAnimals() {
 
         let animals = await human.getAnimals()
 
-        for (let k=0; k < animals.lenth; k++) {
+        for (let k=0; k < animals.length; k++) {
             let animal = animals[k]
 
-            directory +='- ' + animal.name + ', ' + animal.species + '\n'
+            directory +=('- ' + animal.name + ', ' + animal.species + '\n')
         }
     }
-
-    directory = directory.slice(0, -1)
+console.log(directory)
+    // directory = directory.slice(0, -1)
 
     return directory
 }
+// printHumansAndAnimals()
 
 // Return a Set containing the full names of all humans
 // with animals of the given species.
